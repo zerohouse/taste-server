43a6a0c013b01997782fb6010deb990c3cb38b25
dev <parksungho86@gmail.com>
2016-04-26 20:56:10 +0900

    match dto added

--- src/main/java/at/begin/web/user/UserMatchDto.java
+++ src/main/java/at/begin/web/user/UserMatchDto.java
@@ -1,7 +1,5 @@
 package at.begin.web.user;
 
-
-import at.begin.web.chat.ChatDto;
 import at.begin.web.content.ContentDto;
 import at.begin.web.content.book.BookDto;
 import at.begin.web.content.movie.MovieDto;
@@ -14,16 +12,11 @@ import java.util.stream.Collectors;
 
 @Getter
 @Setter
-public class UserDto extends UserCoreDto {
-
+public class UserMatchDto extends UserCoreDto{
     List<ContentDto> contents;
-    List<ChatDto> chats;
-    List<UserCoreDto> matchedUsers;
 
-    public UserDto(User user) {
+    public UserMatchDto(User user) {
         super(user);
-        chats = user.getAllChats().stream().map(ChatDto::new).collect(Collectors.toList());
-        matchedUsers = user.userMatchingUsers.stream().map(userMatchedUser -> new UserMatchDto(userMatchedUser.getMatched())).collect(Collectors.toList());
         contents = user.userLikesContents.stream().map(userLikesContent -> {
             if (userLikesContent.getMusic() != null)
                 return new MusicDto(userLikesContent);
